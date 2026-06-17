7d9ec89e8c7060b6756403a4ae62381100a6636c
Joaquin Moreno <joaquinmoreno1781@gmail.com>
2026-06-17 15:44:42 -0300

    Update funciones.py

--- funciones.py
+++ funciones.py
@@ -1,1 +1,6 @@
-from validaciones import validar_numero
+from validaciones import *
+def menu():
+        print("Bienvenido al menú principal")
+        print("1. Agregar país")
+        print("2. Buscar país")
+        print("3. Salir")
